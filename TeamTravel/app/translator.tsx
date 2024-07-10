bb53205d246f599b5ab744e8500f2ae6a82a15ef
Yvonne <s2210456037@fhooe.at>
2024-07-10 14:10:23 +0200

    added api connection

--- TeamTravel/app/translator.tsx
+++ TeamTravel/app/translator.tsx
@@ -1,23 +1,68 @@
-import {View, Text, StyleSheet, TextInput, Button} from "react-native";
+import {View, Text, StyleSheet, TextInput, Button, ActivityIndicator} from "react-native";
 import {globalStyles} from "../styles/global";
 import {useState} from "react";
 import TranslateInput from "../components/translateInput";
 
+type Translation = {
+    text: string
+}
 
-export default function Translator (props:TranslateProps){
-    //submit aktion für Translate button
-    const submitHandler = (text: string) => {
+
+export default function Translator() {
+
+    const [translation, setTranslation] = useState<Translation[]>([
+        {
+            text: "Translation will appear here"
+        }]);
+
+    /*zeigen, dass daten gerade laden*/
+    const [loadingSpinner, setSpinner] = useState(false);
+}
+//submit aktion für Translate button
+const submitHandler = (text: string, src_lang: string, trgt_lang: string) => {
+
+    if (text != "") {
+        const origin_text = text;
+        console.log(origin_text);
+
+        const source_lang = src_lang;
+        console.log(source_lang);
+
+        const target_lang = trgt_lang;
+        console.log(target_lang);
+
+        /*show spinner*/
+        setSpinner(true);
+
         //übersetzen des Textes
 
         //Aufruf der Übersetzungs API mit gewählter Sprache
 
+        const getTranslationFromApi = () => {
+            return fetch(`https://api-free.deepl.com/v2/translate?auth_key=6b8228fb-b44e-490e-b596-6901359a65ae:fx&text=${origin_text}&source_lang=${source_lang}&target_lang=${target_lang}`)
+                .then(response => response.json())
+                .then(json => {
+                    /* Filtern und Daten setzen mit neuem Wetter */
+                    const translationItems = {
+                        text: json.translations.text
+                    };
+
+                    console.log(translationItems.text);
+
+                    setTranslation([translationItems]);
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        };
 
     }
 
-    return(
+    return (
         <View style={styles.container}>
             <Text>Translate your text here</Text>
             <TranslateInput submitHandler={submitHandler}/>
+            <Text></Text>
         </View>
     );
 }
