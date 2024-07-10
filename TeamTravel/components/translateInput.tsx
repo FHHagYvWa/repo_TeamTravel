f511bf729af731cb5d5ad6766c39c33b7dbee4a6
Yvonne <s2210456037@fhooe.at>
2024-07-10 19:39:15 +0200

    Update translateInput.tsx

--- TeamTravel/components/translateInput.tsx
+++ TeamTravel/components/translateInput.tsx
@@ -5,13 +5,12 @@ import {Picker} from '@react-native-picker/picker';
 import {MaterialIcons} from "@expo/vector-icons";
 
 type TranslateProps = {
-    submitHandler: (text:string, src_lang:string, target_lang:string) => void;
+    submitHandler: (text: string, src_lang: string, target_lang: string) => void;
 }
 
-export default function TranslateInput (props:TranslateProps){
-    //const für text in Inputfeld
+export default function TranslateInput(props: TranslateProps) {
+    // const für text in Inputfeld
     const [text, setText] = useState('');
-
 
     //const für select picker der Ausgangssprache
     const [startLanguage, setStartLanguage] = useState("EN");
@@ -19,28 +18,27 @@ export default function TranslateInput (props:TranslateProps){
     //const für select picker der Zielsprache
     const [selectedLanguage, setSelectedLanguage] = useState("ES");
 
+    // Eingabe löschen, wenn Mülltonnen-Icon geklickt wird
     const handleDelete = () => {
         setText('');
     };
 
-    return(
+    return (
         <View>
-            <View style={styles.searchContainer}>
-
+            <View>
                 <View>
                     <Text style={styles.label}>Translate from:</Text>
                     <View style={styles.pickerStyle}>
-
                         <Picker
                             selectedValue={startLanguage}
                             onValueChange={(itemValue, itemIndex) =>
                                 setStartLanguage(itemValue)
                             }>
-                            <Picker.Item label="English" value="EN" />
-                            <Picker.Item label="German" value="DE" />
-                            <Picker.Item label="Spanish" value="ES" />
-                            <Picker.Item label="French" value="FR" />
-                            <Picker.Item label="Italian" value="IT" />
+                            <Picker.Item label="English" value="EN"/>
+                            <Picker.Item label="German" value="DE"/>
+                            <Picker.Item label="Spanish" value="ES"/>
+                            <Picker.Item label="French" value="FR"/>
+                            <Picker.Item label="Italian" value="IT"/>
                         </Picker>
                     </View>
                     <Text style={styles.label}>Translate to:</Text>
@@ -50,29 +48,26 @@ export default function TranslateInput (props:TranslateProps){
                             onValueChange={(itemValue, itemIndex) =>
                                 setSelectedLanguage(itemValue)
                             }>
-                            <Picker.Item label="English" value="EN" />
-                            <Picker.Item label="German" value="DE" />
-                            <Picker.Item label="Spanish" value="ES" />
-                            <Picker.Item label="French" value="FR" />
-                            <Picker.Item label="Italian" value="IT" />
+                            <Picker.Item label="English" value="EN"/>
+                            <Picker.Item label="German" value="DE"/>
+                            <Picker.Item label="Spanish" value="ES"/>
+                            <Picker.Item label="French" value="FR"/>
+                            <Picker.Item label="Italian" value="IT"/>
                         </Picker>
                     </View>
-
                 </View>
-
                 <View>
-                    <TextInput multiline = {true}
-                               numberOfLines = {5} style={globalStyles.translateInput} placeholder={'Your text...'}
+                    <TextInput multiline={true}
+                               numberOfLines={5} style={globalStyles.translateInput} placeholder={'Your text...'}
                                value={text} onChangeText={setText}/>
                     <TouchableOpacity onPress={handleDelete}>
                         <MaterialIcons style={globalStyles.icon} name="delete" size={30} color={'#000'}/>
                     </TouchableOpacity>
-                    <Button  title='Translate' color='#ffc50a' onPress={()=>props.submitHandler(text, startLanguage, selectedLanguage)}/>
+                    <Button title='Translate' color='#ffc50a'
+                            onPress={() => props.submitHandler(text, startLanguage, selectedLanguage)}/>
                 </View>
-
             </View>
         </View>
-
     );
 }
 
@@ -84,7 +79,6 @@ const styles = StyleSheet.create({
         borderRadius: 5,
         backgroundColor: '#fff',
     },
-
     label: {
         fontSize: 16,
         marginBottom: 8,
